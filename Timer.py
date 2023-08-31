c73da1d850301c013dceddccd25dd7eed47f17f7
Victor Freire <69279495+devbuda@users.noreply.github.com>
2023-08-31 17:31:54 -0400

    Create Timer.py

---
+++ Timer.py
@@ -0,0 +1,20 @@
+from time import sleep
+
+t = input("Informe o tempo(em segundos): ")
+
+if t.isdigit():
+    t = int(t)
+else:
+    print("Entrada inválida")
+    quit()
+
+while t:
+    minutes, seconds = divmod(t, 60)
+   
+    timer = f"{minutes:02d}:{seconds:02d}"
+
+    print(timer, end = "\r")
+    sleep(1)
+    t -= 1
+
+print("\nTemporizador finalizado.\n")
