336f4a59d340d9dc68f8ef72f53bb7915e538b74
Victor Freire <69279495+devbuda@users.noreply.github.com>
2023-09-05 17:48:22 -0400

    Create Password_Authentication.py

---
+++ Password_Authentication.py
@@ -0,0 +1,13 @@
+import getpass
+
+database = {"Administrator": "12345678", "User": "87654321"}
+username = str(input("Enter yout username: "))
+password = getpass.getpass("Enter your password: ")
+
+for i in database.keys():
+    if username == i:
+        while password != database.get(i):
+            print("The password is incorrect.")
+            password = getpass.getpass("Enter your password again: ")
+            break
+        print("Verified")
